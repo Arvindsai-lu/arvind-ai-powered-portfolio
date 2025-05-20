91200336e7f7a7ead61408ff34378fd7fbf91638
Arvindsai-lu <arvindsai1a2b@gmail.com>
2025-05-20 00:37:14 -0500

    new changes
    modified some code

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -7,19 +7,48 @@ import { HiDocumentDuplicate } from 'react-icons/hi'
 import { BiMessageSquareDetail } from 'react-icons/bi'
 import { useState } from 'react'
 
-
-
 const Nav = () => {
-    const [activeNav, setActiveNav] = useState('#')
-    return (
-        <nav>
-            <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeHeart /></a>
-            <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUser /></a>
-            <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''} ><BiBookBookmark /></a>
-            <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><HiDocumentDuplicate /></a>
-            <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
-        </nav>
-    )
+  const [activeNav, setActiveNav] = useState('#hero') // Updated default section
+
+  return (
+    <nav>
+      <a
+        href="#hero"
+        onClick={() => setActiveNav('#hero')}
+        className={activeNav === '#hero' ? 'active' : ''}
+      >
+        <BiHomeHeart />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav('#about')}
+        className={activeNav === '#about' ? 'active' : ''}
+      >
+        <BiUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav('#experience')}
+        className={activeNav === '#experience' ? 'active' : ''}
+      >
+        <BiBookBookmark />
+      </a>
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav('#portfolio')}
+        className={activeNav === '#portfolio' ? 'active' : ''}
+      >
+        <HiDocumentDuplicate />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav('#contact')}
+        className={activeNav === '#contact' ? 'active' : ''}
+      >
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
+  )
 }
 
 export default Nav
